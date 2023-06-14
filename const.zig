be3e816e83db1550f70442eebd8b550e57ea254d
mellero <m_ellero@hotmail.com>
2023-06-13 23:13:07 -0400

    Get CPU.zig compiling properly

--- const.zig
+++ const.zig
@@ -1,37 +1,37 @@
 // Register addresses
-const IDX_A = (0x00);
-const IDX_B = (0x01);
-const IDX_C = (0x02);
-const IDX_D = (0x03);
-const IDX_E = (0x04);
-const IDX_H = (0x05);
-const IDX_L = (0x06);
+pub const IDX_A = (0x00);
+pub const IDX_B = (0x01);
+pub const IDX_C = (0x02);
+pub const IDX_D = (0x03);
+pub const IDX_E = (0x04);
+pub const IDX_H = (0x05);
+pub const IDX_L = (0x06);
 
 // States
-const T1      = (0b010);
-const T1B     = (0b011);
-const T2      = (0b001);
-const WAIT    = (0b000);
-const T3      = (0b100);
-const STOPPED = (0b110);
-const T4      = (0b111);
-const T5      = (0b101);
-const HALT    = (0b00000000);
+pub const T1 = (0b010);
+pub const T1B = (0b011);
+pub const T2 = (0b001);
+pub const WAIT = (0b000);
+pub const T3 = (0b100);
+pub const STOPPED = (0b110);
+pub const T4 = (0b111);
+pub const T5 = (0b101);
+pub const HALT = (0b00000000);
 
 // Flags
-const FLAG_BIT_C = (0x01);
-const FLAG_BIT_P = (0x02);
-const FLAG_BIT_Z = (0x04);
-const FLAG_BIT_S = (0x08);
-const FLAG_BITS_ALL = (0x0F);
-const FLAG_BITS_NONE = (0x00);
+pub const FLAG_BIT_C = (0x01);
+pub const FLAG_BIT_P = (0x02);
+pub const FLAG_BIT_Z = (0x04);
+pub const FLAG_BIT_S = (0x08);
+pub const FLAG_BITS_ALL = (0x0F);
+pub const FLAG_BITS_NONE = (0x00);
 
 // Stack addresses
-const STCK_AS0 = (0x00);
-const STCK_AS1 = (0x01);
-const STCK_AS2 = (0x02);
-const STCK_AS3 = (0x03);
-const STCK_AS4 = (0x04);
-const STCK_AS5 = (0x05);
-const STCK_AS6 = (0x06);
-const STCK_AS7 = (0x07);
+pub const STCK_AS0 = (0x00);
+pub const STCK_AS1 = (0x01);
+pub const STCK_AS2 = (0x02);
+pub const STCK_AS3 = (0x03);
+pub const STCK_AS4 = (0x04);
+pub const STCK_AS5 = (0x05);
+pub const STCK_AS6 = (0x06);
+pub const STCK_AS7 = (0x07);
--- main.zig
+++ main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
+const cpu = @import("cpu.zig");
 
 pub fn main() !void {
-	const stdout = std.io.getStdOut().writer();
-	try stdout.print("Hello, {s}!\n", .{"world"});
+    const stdout = std.io.getStdOut().writer();
+    _ = stdout;
 
+    // Program
+    var opCyclesRemaining: u8 = 0;
+    _ = opCyclesRemaining;
+    var c: cpu.CPU = cpu.initCPU();
+
+    // Print
+    inline for (std.meta.fields(@TypeOf(c))) |f| {
+        std.log.debug(f.name ++ " {any}", .{@as(f.type, @field(c, f.name))});
+    }
 }
