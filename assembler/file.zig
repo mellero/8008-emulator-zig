1abcbcb5d93f5db38e0b35516fab0e2a73241029
mellero <m_ellero@hotmail.com>
2023-06-20 12:15:03 -0400

    Rename variables/functions in file.zig to align with naming conventions

--- assembler/file.zig
+++ assembler/file.zig
@@ -7,29 +7,29 @@ const OpenMode = File.OpenMode;
 /// Output binary
 ///
 pub fn readInputFile() !void {
-    const inputFileName = "assembler/program.txt";
-    const inputFile = try std.fs.cwd().openFile(inputFileName, .{});
-    defer inputFile.close();
+    const input_file_name = "assembler/program.txt";
+    const input_file = try std.fs.cwd().openFile(input_file_name, .{});
+    defer input_file.close();
 
-    const outputFileName = "assembler/output.txt";
-    const outputFile = try std.fs.cwd().createFile(outputFileName, .{});
-    defer outputFile.close();
+    const output_file_name = "assembler/output.txt";
+    const output_file = try std.fs.cwd().createFile(output_file_name, .{});
+    defer output_file.close();
 
-    var buf_reader = std.io.bufferedReader(inputFile.reader());
+    var buf_reader = std.io.bufferedReader(input_file.reader());
     var in_stream = buf_reader.reader();
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        if (split_line(line)) |splitLine| {
-            if (splitLine.len == 0) {
+        if (splitLine(line)) |split_line| {
+            if (split_line.len == 0) {
                 // blank line
                 continue;
             }
-            if (try line_to_binary(splitLine)) |binary_rep| {
-                try write_to_output(outputFile, binary_rep);
-                try log_line_binary(line, binary_rep);
+            if (try lineToBinary(split_line)) |binary_rep| {
+                try writeToOutput(output_file, binary_rep);
+                try logLineToBinary(line, binary_rep);
             } else {
-                std.log.debug("Bad input line {s}", .{line});
+                std.log.debug(">> Bad input line {s}", .{line});
             }
         }
     }
@@ -42,41 +42,41 @@ const TripleInst = struct { opCode: BinaryByte, memLow: BinaryByte, memHigh: Bin
 const InstTag = enum { single, double, triple };
 const BinInst = union(InstTag) { single: SingleInst, double: DoubleInst, triple: TripleInst };
 
-fn line_to_binary(splitLine: [][]u8) !?BinInst {
-    const opCodeHex = opCodeToHexMap.get(splitLine[0]) orelse return null;
-    const opBin = try convert_num_to_bin(opCodeHex);
+fn lineToBinary(split_line: [][]u8) !?BinInst {
+    const op_code_hex = opCodeToHexMap.get(split_line[0]) orelse return null;
+    const op_bin = try convertToBin(op_code_hex);
 
-    switch (splitLine.len) {
-        1 => return BinInst{ .single = .{ .opCode = opBin } },
+    switch (split_line.len) {
+        1 => return BinInst{ .single = .{ .opCode = op_bin } },
         2 => {
-            var imm = splitLine[1];
-            const immBin = try convert_num_to_bin(imm);
-            return BinInst{ .double = .{ .opCode = opBin, .immVal = immBin } };
+            var imm = split_line[1];
+            const imm_bin = try convertToBin(imm);
+            return BinInst{ .double = .{ .opCode = op_bin, .immVal = imm_bin } };
         },
         3 => {
-            var low = splitLine[1];
-            var high = splitLine[2];
-            const lowBits = try convert_num_to_bin(low);
-            const highBits = try convert_num_to_bin(high);
-            return BinInst{ .triple = .{ .opCode = opBin, .memLow = lowBits, .memHigh = highBits } };
+            var low = split_line[1];
+            var high = split_line[2];
+            const low_bits = try convertToBin(low);
+            const high_bits = try convertToBin(high);
+            return BinInst{ .triple = .{ .opCode = op_bin, .memLow = low_bits, .memHigh = high_bits } };
         },
         else => return null,
     }
 }
 
-fn split_line(line: []u8) ?[][]u8 {
+fn splitLine(line: []u8) ?[][]u8 {
     var arr: [3][]u8 = undefined;
-    const lineTrimmed = std.mem.trim(u8, line, " ");
-    var spliterator = std.mem.splitSequence(u8, lineTrimmed, " ");
+    const line_trimmed = std.mem.trim(u8, line, " ");
+    var spliterator = std.mem.splitSequence(u8, line_trimmed, " ");
 
     var i: u8 = 0;
-    while (spliterator.next()) |splitValue| {
+    while (spliterator.next()) |split_val| {
         if (i > 2) {
             // Invalid - TODO: handle case better
             return null;
         }
 
-        var trimmed = std.mem.trim(u8, splitValue, " ");
+        var trimmed = std.mem.trim(u8, split_val, " ");
 
         if (trimmed.len > 0) {
             arr[i] = @constCast(trimmed);
@@ -92,28 +92,28 @@ fn split_line(line: []u8) ?[][]u8 {
     return arr[0..i];
 }
 
-test "split_line should return null on empty string" {
-    const blankInput = "";
-    const blankResult = split_line(blankInput);
-    try std.testing.expect(blankResult == null);
+test "splitLine should return null on empty string" {
+    const blank_input = "";
+    const blank_result = splitLine(blank_input);
+    try std.testing.expect(blank_result == null);
 
-    const manySpaceInput = "            "[0..];
-    const manySpaceResult = split_line(@constCast(manySpaceInput));
-    try std.testing.expect(manySpaceResult == null);
+    const many_space_input = "            "[0..];
+    const many_space_result = splitLine(@constCast(many_space_input));
+    try std.testing.expect(many_space_result == null);
 }
 
-test "split_line should return proper split on valid string" {
+test "splitLine should return proper split on valid string" {
     // Not a great test, just to keep API honest for now
     const input = "ABA 123123"[0..];
-    if (split_line(@constCast(input))) |result| {
+    if (splitLine(@constCast(input))) |result| {
         const op = result[0];
-        const rest = result[1];
+        const imm = result[1];
         try std.testing.expect(std.mem.eql(u8, op, input[0..3]));
-        try std.testing.expect(std.mem.eql(u8, rest, input[4..]));
+        try std.testing.expect(std.mem.eql(u8, imm, input[4..]));
     }
 }
 
-fn write_to_output(file: File, inst: BinInst) !void {
+fn writeToOutput(file: File, inst: BinInst) !void {
     const writer = file.writer();
     switch (inst) {
         BinInst.single => |value| try std.fmt.format(writer, "|{s}|", .{value.opCode}),
@@ -122,9 +122,9 @@ fn write_to_output(file: File, inst: BinInst) !void {
     }
 }
 
-fn log_line_binary(line: []u8, inst: BinInst) !void {
-    var outputBuf: [1024]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&outputBuf);
+fn logLineToBinary(line: []u8, inst: BinInst) !void {
+    var output_buf: [1024]u8 = undefined;
+    var fbs = std.io.fixedBufferStream(&output_buf);
     var writer = fbs.writer();
 
     switch (inst) {
@@ -156,9 +156,9 @@ const opCodeToHexMap = std.ComptimeStringMap(*const [4:0]u8, .{
     .{ "JMP", "0xCE" },
 });
 
-fn convert_num_to_bin(input: []const u8) ![8:0]u8 {
-    var outputBuf: [9]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&outputBuf);
+fn convertToBin(input: []const u8) ![8:0]u8 {
+    var output_buf: [9]u8 = undefined;
+    var fbs = std.io.fixedBufferStream(&output_buf);
     var writer = fbs.writer();
 
     const result = try std.fmt.parseInt(u8, input, 0);
@@ -169,18 +169,18 @@ fn convert_num_to_bin(input: []const u8) ![8:0]u8 {
     return ptr.*;
 }
 
-test "convert_num_to_bin should return proper results on proper input" {
+test "convertToBin should return proper results on proper input" {
     const expected = "00010001";
 
-    var inputHex = "0x11".*;
-    const result = try convert_num_to_bin(&inputHex);
+    var input_hex = "0x11".*;
+    const result = try convertToBin(&input_hex);
     try std.testing.expect(std.mem.eql(u8, expected, result[0..]));
 
-    var inputDecimal = "17".*;
-    const resultDecimal = try convert_num_to_bin(&inputDecimal);
-    try std.testing.expect(std.mem.eql(u8, expected, resultDecimal[0..]));
+    var input_decimal = "17".*;
+    const result_decimal = try convertToBin(&input_decimal);
+    try std.testing.expect(std.mem.eql(u8, expected, result_decimal[0..]));
 
-    var inputBinary = "0b10001".*;
-    const resultBinary = try convert_num_to_bin(&inputBinary);
-    try std.testing.expect(std.mem.eql(u8, expected, resultBinary[0..]));
+    var input_bin = "0b10001".*;
+    const result_bin = try convertToBin(&input_bin);
+    try std.testing.expect(std.mem.eql(u8, expected, result_bin[0..]));
 }
